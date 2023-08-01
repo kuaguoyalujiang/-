88c39d4986c36acc57333c14e199ca4335fd82f0
wsy926474968 <55973506+wsy926474968@users.noreply.github.com>
2023-08-02 04:17:39 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,16 @@
+{
+  "spider": "https://fongmi.cachefly.net/FongMi/CatVodSpider/main/jar/custom_spider.jar;md5;04cfe6ba366acf7e94a7d44795fc0bc3",
+  "wallpaper": "http://饭太硬.top/深色壁纸/api.php",
+  "sites": [
+  {
+    "key": "lf_js_ikanbot",
+    "name": "点播 | lf_ikanbot",
+    "type": 3,
+    "searchable": 1,
+    "changeable": 1,
+    "quickSearch": 1,
+    "filterable": 1,
+    "api": "./lf_ikanbot3_2_min.js"
+}
+  ]
+}
